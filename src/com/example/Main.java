d78b9ae65e77c3603b3237df2cae6859a95559a4
Will B <tryarc@comcast.net>
2022-10-11 15:49:02 -0400

    add min and max value

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,5 +1,16 @@
+package com.example;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float min value: " + myMinFloatValue);
+        System.out.println("Float max value: " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double min value: " + myMinDoubleValue);
+        System.out.println("Double max value: " + myMaxDoubleValue);
     }
 }
