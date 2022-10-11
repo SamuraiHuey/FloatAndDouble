80b83da9f00ff65c78c6bc239d5bea1e0c818d66
Will B <tryarc@comcast.net>
2022-10-11 15:59:02 -0400

    finish

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -12,5 +12,12 @@ public class Main {
         double myMaxDoubleValue = Double.MAX_VALUE;
         System.out.println("Double min value: " + myMinDoubleValue);
         System.out.println("Double max value: " + myMaxDoubleValue);
+
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00 / 3.00;
+        System.out.println("MyIntValue= " + myIntValue);
+        System.out.println("MyFloatValue= " + myFloatValue);
+        System.out.println("MyDoubleValue= " + myDoubleValue);
     }
 }
